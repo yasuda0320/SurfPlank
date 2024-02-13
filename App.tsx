351859482842a17873a4d15c1d219da6e2fec5c3
yasuda0320 <65007448+yasuda0320@users.noreply.github.com>
2024-02-13 22:14:39 +0900

    Androidでもスライドアニメーション

--- App.tsx
+++ App.tsx
@@ -2,18 +2,24 @@
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 
 import CategoryListScreen from './CategoryListScreen';
 import BoardListScreen from './BoardListScreen';
-import { RootStackParamList } from './types.ts';
+import { RootStackParamList } from './types';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="CategoryList">
+      <Stack.Navigator
+        initialRouteName="CategoryList"
+        screenOptions={{
+          // 画面遷移アニメーションのカスタマイズ
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+        }}
+      >
         <Stack.Screen name="CategoryList" component={CategoryListScreen} options={{ title: 'カテゴリ一覧' }} />
         <Stack.Screen name="BoardList" component={BoardListScreen} options={{ title: '板一覧' }} />
       </Stack.Navigator>
